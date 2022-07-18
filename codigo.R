f69de06f9adef7b3acdd152113a8e7b853edbed1
cedma.santos <cedma.santos@gmail.com>
2022-07-18 10:24:57 -0300

    Passo 03 - carregando dplyr

--- codigo.R
+++ codigo.R
@@ -2,4 +2,13 @@
 
 # Passo 01 - Instalar pacote do r chamadar readr
 
-install.packages("readr")
+instgiall.packages("readr")
+
+# Passo 02 - 
+
+dados <- read.csv("C:/Users/cedma/OneDrive/Documents/R/escolas_ssa/data/Quant_Geral_Alunos_Escola_Regional_2022.csv", 
+    sep = ";")
+
+#passo 03 - Carregar dplyr
+
+library("dplyr")
